2076ec9643511fafd0a3c195de7c55c34229ff6f
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-02-05 23:31:02 +0100

    feat: add svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,47 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'node:path';
 
+import { getSVGSymbolsString } from './scripts/svg';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/sass/style.scss" as *;'
-      }
-    }
+export default defineConfig( async ({}) => {
+  return {
+    plugins: [vue(),
+      createHtmlPlugin({
+        minify: true,
+        entry: 'src/main.ts',
+        template: 'index.html',
+        inject: {
+          data: {
+            injectSVGSymbols: await getSVGSymbolsString(resolve('src/icons'))
+          },
+          tags: [
+            {
+              injectTo: 'body-prepend',
+              tag: 'div',
+              attrs: {
+                id: 'app',
+                class: 'app-container'
+              }
+            }
+          ]
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/sass/style.scss" as *;',
+        },
+      },
+    },
   }
-})
+});
