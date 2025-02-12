4df36c795679656b24807656755b2fb2785a9df2
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-02-12 21:23:13 +0100

    feat: add swiper

---
+++ src/components/ui/UIImageSwiper.vue
@@ -0,0 +1,63 @@
+<template>
+  <swiper :pagination="{
+    type: 'fraction',
+  }" :navigation="true" :modules="modules" class="mySwiper">
+    <swiper-slide v-for="image of images">
+      <UIImage :src="image" :width="width" :height="height"/>
+    </swiper-slide>
+  </swiper>
+</template>
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import UIImage from './UIImage.vue';
+
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+import { Pagination, Navigation } from 'swiper/modules';
+
+export default {
+  name: "UIImageSwiper",
+  components: {
+    Swiper,
+    SwiperSlide,
+    UIImage
+  },
+  props: {
+    images: {
+      type: Array,
+      default: []
+    },
+    width: {
+      type: [Number, String],
+      default: ''
+    },
+    height: {
+      type: [Number, String],
+      default: ''
+    }
+  },
+  setup() {
+    return {
+      modules: [Pagination, Navigation],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.swiper {
+  width: 100%;
+
+}
+
+.swiper-slide {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
